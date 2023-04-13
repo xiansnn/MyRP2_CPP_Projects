d35905c9165876f44f326fbaddd91ab01afedf28
xiansnn <xiansnn@hotmail.com>
2023-04-13 18:55:50 +0200

    add calibration routine

--- mpu6050_i2c/mpu6050.cpp
+++ mpu6050_i2c/mpu6050.cpp
@@ -1,19 +1,38 @@
+#include "config_MPU6050.h"
 #include "mpu6050.h"
 #include "pico/stdlib.h"
-#include "config_MPU6050.h"
 
 MPU6050::MPU6050()
 {
-    this->sample_rate = SAMPLE_RATE;
+    // config I2C
     i2c_init(I2C_BUS, I2C_SPEED);
     gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
     gpio_pull_up(I2C_SCL);
     gpio_pull_up(I2C_SDA);
-    this->set_clock_src_DLPF_and_sample_rate(this->sample_rate, DLPF_BW::HZ_250);
-    this->set_gyro_resolution(GYRO_FULL_SCALE_RANGE::DPS_250);
-    this->set_accel_resolution(ACCEL_FULL_SCALE_RANGE::G_2);
-    this->reset_enable_config_FIFO();
+    // config clock source
+    this->single_byte_write(PWR_MGMT_1_RA, PWR_MGMT_1);
+    // config FSYNC and DLPF config
+    this->single_byte_write(CONFIG_RA, CONFIG);
+    // sample rate = gyro output rate /(1+SMPLRT_DIV)
+    uint8_t sample_div = int((GYRO_OUT_RATE / SAMPLE_RATE) - 1);
+    // this->sample_rate = GYRO_OUT_RATE / (1 + sample_div);
+    this->single_byte_write(SMPLRT_DIV_RA, sample_div);
+    // set gyro sensor resolution
+    this->gyro_factor = (float)GYRO_FULL_SCALE_VALUE / 32768.;
+    this->single_byte_write(GYRO_CONFIG_RA, GYRO_CONFIG);
+    // set acceleration sensor resolution
+    this->acceleration_factor = (float)ACCEL_FULL_SCALE_VALUE / 32768.;
+    this->single_byte_write(ACCEL_CONFIG_RA, ACCEL_CONFIG);
+    // reset FIFO
+    this->single_byte_write(USER_CTRL_RA, FIFO_RESET);
+    sleep_us(250);
+    // enable FIFO
+    this->single_byte_write(USER_CTRL_RA, FIFO_EN);
+    sleep_us(250);
+    // configure sensors to write in FIFO
+    this->single_byte_write(FIFO_EN_RA, FIFO_SELECTED_SENSOR);
+    this->calibrate();
 }
 
 uint8_t MPU6050::single_byte_write(uint8_t reg_addr, uint8_t reg_value)
@@ -37,126 +56,6 @@ uint8_t MPU6050::burst_byte_read(uint8_t reg_addr, uint8_t *dest, uint8_t len)
     uint8_t nb = i2c_read_blocking(I2C_BUS, MPU_ADDR, dest, len, false);
     return nb;
 }
-
-void MPU6050::set_clock_src_DLPF_and_sample_rate(float rate, DLPF_BW bw)
-{
-    // config clock source
-    this->single_byte_write(PWR_MGMT_1_RA, PWR_MGMT_1_CLKSEL);
-    // config FSYNC and DLPF config
-    int gyro_out_rate{};
-    uint8_t dlpf_code{};
-    switch (bw)
-    {
-    case DLPF_BW::HZ_250:
-        gyro_out_rate = 8000; // gyro output rate = 8000Hz when DLPF is disabled 1000Hz otherwise
-        dlpf_code = 0x00;
-        break;
-    case DLPF_BW::HZ_200:
-        gyro_out_rate = 1000;
-        dlpf_code = 0x01;
-        break;
-    case DLPF_BW::HZ_100:
-        gyro_out_rate = 1000;
-        dlpf_code = 0x02;
-        break;
-    case DLPF_BW::HZ_50:
-        gyro_out_rate = 1000;
-        dlpf_code = 0x03;
-        break;
-    case DLPF_BW::HZ_20:
-        gyro_out_rate = 1000;
-        dlpf_code = 0x04;
-        break;
-    case DLPF_BW::HZ_10:
-        gyro_out_rate = 1000;
-        dlpf_code = 0x05;
-        break;
-    case DLPF_BW::HZ_5:
-        gyro_out_rate = 1000;
-        dlpf_code = 0x06;
-    default:
-        gyro_out_rate = 8000;
-        dlpf_code = 0x00;
-        break;
-    }
-    uint8_t ext_sync_set_code = 0x00 << 3; //  external FSYNC input disabled
-    uint8_t config = ext_sync_set_code | dlpf_code;
-    this->single_byte_write(CONFIG_RA, config);
-
-    // sample rate = gyro output rate /(1+SMPLRT_DIV)
-    uint8_t sample_div = int((gyro_out_rate / rate) - 1);
-    this->sample_rate = gyro_out_rate / (1 + sample_div);
-    this->single_byte_write(SMPLRT_DIV_RA, sample_div);
-}
-
-void MPU6050::set_gyro_resolution(GYRO_FULL_SCALE_RANGE range)
-{
-    uint8_t max_gyro_scale_code{};
-    uint16_t max_gyro_scale_value{};
-    switch (range)
-    {
-    case GYRO_FULL_SCALE_RANGE::DPS_250:
-        max_gyro_scale_code = 0x0 << 3;
-        max_gyro_scale_value = 250;
-        break;
-    case GYRO_FULL_SCALE_RANGE::DPS_500:
-        max_gyro_scale_code = 0x1 << 3;
-        max_gyro_scale_value = 500;
-        break;
-    case GYRO_FULL_SCALE_RANGE::DPS_1000:
-        max_gyro_scale_code = 0x2 << 3;
-        max_gyro_scale_value = 1000;
-        break;
-    case GYRO_FULL_SCALE_RANGE::DPS_2000:
-        max_gyro_scale_code = 0x3 << 3;
-        max_gyro_scale_value = 2000;
-        break;
-    }
-    this->gyro_factor = (float)max_gyro_scale_value / 32768.;
-    this->single_byte_write(GYRO_CONFIG_RA, max_gyro_scale_code);
-}
-
-void MPU6050::set_accel_resolution(ACCEL_FULL_SCALE_RANGE range)
-{
-    uint8_t max_g_scale_code{};
-    uint8_t max_g_scale_value{};
-    switch (range)
-    {
-    case ACCEL_FULL_SCALE_RANGE::G_2:
-        max_g_scale_code = 0x00 << 3;
-        max_g_scale_value = 2;
-        break;
-    case ACCEL_FULL_SCALE_RANGE::G_4:
-        max_g_scale_code = 0x01 << 3;
-        max_g_scale_value = 4;
-        break;
-    case ACCEL_FULL_SCALE_RANGE::G_8:
-        max_g_scale_code = 0x02 << 3;
-        max_g_scale_value = 8;
-        break;
-    case ACCEL_FULL_SCALE_RANGE::G_16:
-        max_g_scale_code = 0x03 << 3;
-        max_g_scale_value = 16;
-        break;
-    }
-
-    this->acceleration_factor = (float)max_g_scale_value / 32768;
-    this->single_byte_write(ACCEL_CONFIG_RA, max_g_scale_code);
-}
-
-void MPU6050::read_MPU_all_measure_from_registers(MPUData_t *data)
-{
-    RawData_t raw;
-    this->read_registers_all_raw_data(&raw);
-    data->g_x = raw.g_x * this->acceleration_factor;
-    data->g_y = raw.g_y * this->acceleration_factor;
-    data->g_z = raw.g_z * this->acceleration_factor;
-    data->gyro_x = raw.gyro_x * this->gyro_factor;
-    data->gyro_y = raw.gyro_y * this->gyro_factor;
-    data->gyro_z = raw.gyro_z * this->gyro_factor;
-    data->temp_out = raw.temp_out * this->temperature_gain + this->temperature_offset;
-}
-
 void MPU6050::read_registers_all_raw_data(RawData_t *measures)
 {
     uint8_t read_buf[14];
@@ -182,7 +81,6 @@ void MPU6050::read_FIFO_all_raw_data(RawData_t *measures)
     measures->gyro_y = (read_buf[10] << 8) + read_buf[11];
     measures->gyro_z = (read_buf[12] << 8) + read_buf[13];
 }
-
 void MPU6050::read_FIFO_g_accel_raw_data(RawData_t *measures)
 {
     uint8_t read_buf[12];
@@ -195,13 +93,46 @@ void MPU6050::read_FIFO_g_accel_raw_data(RawData_t *measures)
     measures->gyro_z = (read_buf[10] << 8) + read_buf[11];
 }
 
-void MPU6050::reset_enable_config_FIFO()
+void MPU6050::calibrate()
 {
-    this->single_byte_write(USER_CTRL_RA, USER_CTRL_FIFO_RESET);
-    sleep_us(250);
-    this->single_byte_write(USER_CTRL_RA, USER_CTRL_FIFO_EN);
-    sleep_us(250);
-    this->single_byte_write(FIFO_EN_RA, FIFO_EN_VALUE); // write all MPU data into FIFO, assuming no external sensor
+    int64_t accel_x{};
+    int64_t accel_y{};
+    int64_t accel_z{};
+    int64_t gyro_x{};
+    int64_t gyro_y{};
+    int64_t gyro_z{};
+    int16_t nb_sample = 1024;
+
+    for (int16_t i = 0; i < nb_sample; i++)
+    {
+        sleep_ms(1);
+        RawData_t raw{};
+        this->read_registers_all_raw_data(&raw);
+        accel_x += raw.g_x;
+        accel_y += raw.g_y;
+        accel_z += raw.g_z;
+        gyro_x += raw.gyro_x;
+        gyro_y += raw.gyro_y;
+        gyro_z += raw.gyro_z;
+    }
+    this->accel_x_offset = -(float)accel_x / nb_sample;
+    this->accel_y_offset = -accel_y / nb_sample;
+    this->accel_z_offset = 32768.0 / ACCEL_FULL_SCALE_RANGE - (float)accel_z / nb_sample;
+    this->gyro_x_offset = -(float)gyro_x / nb_sample;
+    this->gyro_y_offset = -(float)gyro_y / nb_sample;
+    this->gyro_z_offset = -(float)gyro_z / nb_sample;
+}
+void MPU6050::read_MPU_all_measure_from_FIFO(MPUData_t *data)
+{
+    RawData_t raw;
+    this->read_FIFO_all_raw_data(&raw);
+    data->g_x = (raw.g_x + this->accel_x_offset) * this->acceleration_factor;
+    data->g_y = (raw.g_y + this->accel_y_offset) * this->acceleration_factor;
+    data->g_z = (raw.g_z + this->accel_z_offset) * this->acceleration_factor;
+    data->gyro_x = (raw.gyro_x + this->gyro_x_offset) * this->gyro_factor;
+    data->gyro_y = (raw.gyro_y + this->gyro_y_offset) * this->gyro_factor;
+    data->gyro_z = (raw.gyro_z + this->gyro_z_offset) * this->gyro_factor;
+    data->temp_out = raw.temp_out * this->temperature_gain + this->temperature_offset;
 }
 
 float MPU6050::read_MPU_temperature()
@@ -211,37 +142,36 @@ float MPU6050::read_MPU_temperature()
     int16_t temp_out = (read_buf[0] << 8) + read_buf[1];
     return (float)temp_out * this->temperature_gain + this->temperature_offset;
 }
+void MPU6050::read_MPU_g_accel_measures_from_FIFO(MPUData_t *data)
+{
+    RawData_t raw{};
+    this->read_FIFO_g_accel_raw_data(&raw);
+    data->g_x = (raw.g_x + this->accel_x_offset) * this->acceleration_factor;
+    data->g_y = (raw.g_y + this->accel_y_offset) * this->acceleration_factor;
+    data->g_z = (raw.g_z + this->accel_z_offset) * this->acceleration_factor;
+    data->gyro_x = (raw.gyro_x + this->gyro_x_offset) * this->gyro_factor;
+    data->gyro_y = (raw.gyro_y + this->gyro_y_offset) * this->gyro_factor;
+    data->gyro_z = (raw.gyro_z + this->gyro_z_offset) * this->gyro_factor;
+}
+
+void MPU6050::read_MPU_all_measure_from_registers(MPUData_t *data)
+{
+    RawData_t raw;
+    this->read_registers_all_raw_data(&raw);
+    data->g_x = (raw.g_x + this->accel_x_offset) * this->acceleration_factor;
+    data->g_y = (raw.g_y + this->accel_y_offset) * this->acceleration_factor;
+    data->g_z = (raw.g_z + this->accel_z_offset) * this->acceleration_factor;
+    data->gyro_x = (raw.gyro_x + this->gyro_x_offset) * this->gyro_factor;
+    data->gyro_y = (raw.gyro_y + this->gyro_y_offset) * this->gyro_factor;
+    data->gyro_z = (raw.gyro_z + this->gyro_z_offset) * this->gyro_factor;
+    data->temp_out = raw.temp_out * this->temperature_gain + this->temperature_offset;
+}
 
 uint16_t MPU6050::read_FIFO_count()
 {
     uint8_t read_buf[2] = {FIFO_COUNT_H_RA};
     this->burst_byte_read(FIFO_COUNT_H_RA, read_buf, 2);
     return (read_buf[0] << 8) + read_buf[1];
-}
-
-void MPU6050::read_MPU_all_measure_from_FIFO(MPUData_t *data)
-{
-    RawData_t raw;
-    this->read_FIFO_all_raw_data(&raw);
-    data->g_x = raw.g_x * this->acceleration_factor;
-    data->g_y = raw.g_y * this->acceleration_factor;
-    data->g_z = raw.g_z * this->acceleration_factor;
-    data->gyro_x = raw.gyro_x * this->gyro_factor;
-    data->gyro_y = raw.gyro_y * this->gyro_factor;
-    data->gyro_z = raw.gyro_z * this->gyro_factor;
-    data->temp_out = raw.temp_out * this->temperature_gain + this->temperature_offset;
-}
-
-void MPU6050::read_MPU_g_accel_measures_from_FIFO(MPUData_t *data)
-{
-    RawData_t raw{};
-    this->read_FIFO_g_accel_raw_data(&raw);
-    data->g_x = raw.g_x * this->acceleration_factor;
-    data->g_y = raw.g_y * this->acceleration_factor;
-    data->g_z = raw.g_z * this->acceleration_factor;
-    data->gyro_x = raw.gyro_x * this->gyro_factor;
-    data->gyro_y = raw.gyro_y * this->gyro_factor;
-    data->gyro_z = raw.gyro_z * this->gyro_factor;
 }
 
 uint8_t MPU6050::read_interrupt_status()
--- mpu6050_i2c/test_mpu6050.cpp
+++ mpu6050_i2c/test_mpu6050.cpp
@@ -5,34 +5,39 @@
 #include <stdio.h>
 #include <math.h>
 
+void process_data(MPU6050 mpu){
+    MPUData_t measures{};
+    mpu.read_MPU_g_accel_measures_from_FIFO(&measures);
+    printf("fifo AccX = %.2f\tY = %.2f\tZ = %.2f", measures.g_x, measures.g_y, measures.g_z);
+    printf("\tvecteur G: %.3f", sqrt(pow(measures.g_x, 2) + pow(measures.g_y, 2) + pow(measures.g_z, 2)));
+    printf("\tGyroX = %.2f\tY = %.2f\tZ = %.2f", measures.gyro_x, measures.gyro_y, measures.gyro_z);
+    printf("\n\n");
+
+};
+void process_raw_data(MPU6050 mpu){
+    RawData_t raw{};
+    mpu.read_FIFO_g_accel_raw_data(&raw);
+    printf("fifo AccX = %d\tY = %d\tZ = %d", raw.g_x, raw.g_y, raw.g_z);
+    // printf("\tvecteur G: %d", sqrt(pow(raw.g_x, 2) + pow(raw.g_y, 2) + pow(raw.g_z, 2)));
+    printf("\tGyroX = %d\tY = %d\tZ = %d", raw.gyro_x, raw.gyro_y, raw.gyro_z);
+    printf("\n\n");
+};
+
 int main()
 {
     stdio_init_all();
     MPU6050 mpu = MPU6050();
-    sleep_ms(100);
+    // measures.temp_out = mpu.read_MPU_temperature();
+    // printf("\t\tTemp. = %.2f\n", measures.temp_out);
+    // sleep_ms(100);
     while (true)
     {
-        uint16_t count = mpu.read_FIFO_count();
-        if (count > 0)
+        while (mpu.read_FIFO_count() > 0)
         {
-            MPUData_t measures = mpu.read_MPU_measure_from_registers();
-            printf("register Acc. X = %.2f, Y = %.2f, Z = %.2f", measures.g_x, measures.g_y, measures.g_z);
-            float g = pow(measures.g_x, 2) + pow(measures.g_y, 2) + pow(measures.g_z, 2);
-            printf("\tvecteur G: %.3f", g);
-            printf("\tGyro. X = %.2f Y = %.2f, Z = %.2f", measures.gyro_x, measures.gyro_y, measures.gyro_z);
-            printf("\t\tTemp. = %.2f\n", measures.temp_out);
-            
-            measures = mpu.read_MPU_measure_from_FIFO();
-            printf("fifo Acc. X = %.2f, Y = %.2f, Z = %.2f", measures.g_x, measures.g_y, measures.g_z);
-            g = pow(measures.g_x, 2) + pow(measures.g_y, 2) + pow(measures.g_z, 2);
-            printf("\tvecteur G: %.3f", g);
-            printf("\tGyro. X = %.2f, Y = %.2f, Z = %.2f", measures.gyro_x, measures.gyro_y, measures.gyro_z);
-            printf("\t\tTemp. = %.2f\n", measures.temp_out);
-            printf("\n\n");
-            
-        } 
+            // process_raw_data(mpu);
+            process_data(mpu);     
+        }
         sleep_ms(5);
     }
-
     return 0;
 }
