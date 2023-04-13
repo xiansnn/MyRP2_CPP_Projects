64a7bc7c9beedc54a8da0729a426153cc95e93f7
xiansnn <xiansnn@hotmail.com>
2023-04-13 18:55:34 +0200

    all M6050 config is done at compile time

--- mpu6050_i2c/config_MPU6050.h
+++ mpu6050_i2c/config_MPU6050.h
@@ -1,83 +1,165 @@
-//---I2C bus config---
-//--------------------
+#if !defined(CONFIG_MPU6050_H)
+#define CONFIG_MPU6050_H
+
+#include "register_address_MPU6050.h"
+
+//----------------------------------------------------I2C bus config---
+//---------------------------------------------------------------------
+// set I2C channel 0 or 1
 #define I2C_BUS i2c0
 // #define I2C_BUS i2c1
+// set I2C speed Fast or Standard
 // #define I2C_SPEED 100000 // I2C STANDARD-MODE
 #define I2C_SPEED 400000 // I2C FAST-MODE
+// set I2C SDA and SCL pin
 #define I2C_SDA 8 // gpio pin on MakerFab Primer Board
 #define I2C_SCL 9 // gpio pin on MakerFab Primer Board
 
-//---define MPU config---
-//-----------------------
-#define MPU_ADDR 0x68  // assuming AD0 pin is low
-// define sensors sample rate
-#define SAMPLE_RATE 50.0 // in Hz
-#define SMPLRT_DIV_RA 0x19
-// config FSYNC and DLPF config
-#define CONFIG_RA 0x1A
-// config Gyro self test and full scale
-#define GYRO_CONFIG_RA 0x1B
-// config Accelerator self test and full scale
-#define ACCEL_CONFIG_RA 0x1C// define MPU clock source
-// config which sensors are loaded into FIFO
-#define FIFO_EN_RA 0x23 // a 1024-byte FIFO
-#define FIFO_EN_VALUE ALL_GYRO | FIFO_EN_ACCEL_FIFO_EN
-// #define FIFO_EN_VALUE FIFO_EN_TEMP_FIFO_EN | FIFO_EN_ACCEL_FIFO_EN | ALL_GYRO
-#define ALL_GYRO FIFO_EN_XG_FIFO_EN | FIFO_EN_YG_FIFO_EN | FIFO_EN_ZG_FIFO_EN
-#define FIFO_EN_TEMP_FIFO_EN 0x80
-#define FIFO_EN_XG_FIFO_EN 0x40
-#define FIFO_EN_YG_FIFO_EN 0x20
-#define FIFO_EN_ZG_FIFO_EN 0x10
-#define FIFO_EN_ACCEL_FIFO_EN 0x08
-#define FIFO_EN_SLV2_FIFO_EN 0x04
-#define FIFO_EN_SLV1_FIFO_EN 0x02
-#define FIFO_EN_SLV0_FIFO_EN 0x01
-// config Interrupt pin mode and I2C ByPass enable
-#define INT_PIN_CFG_RA 0x37
-#define INT_PIN_CFG_INT_LEVEL 0x80
-#define INT_PIN_CFG_INT_OPEN 0x40
-#define INT_PIN_CFG_LATCH_INT_EN 0x20
-#define INT_PIN_CFG_INT_RD_CLEAR 0x10
-#define INT_PIN_CFG_FSYNC_INT_LEVEL 0x08
-#define INT_PIN_CFG_FSYNC_INT_EN 0x04
-#define INT_PIN_CFG_I2C_BYPASS_EN 0x02
-// config Interrupt enable
-#define INT_ENABLE_RA 0x38
-#define INT_ENABLE_FIFO_OFLOW_EN 0x10
-#define INT_ENABLE_I2C_MST_INT_EN 0x08
-#define INT_ENABLE_DATA_RDY_EN 0x01
-// read Interrupt status
-#define INT_STATUS_RA 0x3A
-#define INT_STATUS_FIFO_OFLOW_INT 0x10
-#define INT_STATUS_I2C_MST_INT 0x08
-#define INT_STATUS_DATA_RDY_INT 0x01
-// measures registers
-#define ACCEL_XOUT_H_RA 0x3B // the first data serves as a base for all MPU measures
-#define TEMP_OUT_H_RA 0x41
-// signal path reset
-#define SIGNAL_PATH_RESET_RA 0x68
-#define SIGNAL_PATH_RESET_GYRO_RESET 0x04
-#define SIGNAL_PATH_RESET_ACCEL_RESET 0x02
-#define SIGNAL_PATH_RESET_TEMP_RESET 0x01
-// config FIFO, I2C, signal path
-#define USER_CTRL_RA 0x6A
-#define USER_CTRL_FIFO_EN 0x40
-#define USER_CTRL_I2C_MST_EN 0x20
-#define USER_CTRL_I2C_IF_DIS 0x10 // MPU6000 only
-#define USER_CTRL_FIFO_RESET 0x04
-#define USER_CTRL_I2C_MST_RESET 0x02
-#define USER_CTRL_SIG_COND_RESET 0x01
-// config power, reset, sleep mode
-#define PWR_MGMT_1_RA 0x6B
-#define PWR_MGMT_1_CLKSEL PWR_MGMT_1_CLKSEL_INTERNAL
-#define PWR_MGMT_1_CLKSEL_INTERNAL 0x00
-#define PWR_MGMT_1_CLKSEL_X_PLL 0x01
-#define PWR_MGMT_1_CLKSEL_Y_PLL 0x02
-#define PWR_MGMT_1_CLKSEL_Z_PLL 0x03
-// FIFO count register
-#define FIFO_COUNT_H_RA 0x72
-// measures FIFO entry
-#define FIFO_R_W_RA 0x74
-// MPU address default register
-#define WHO_AM_I_RA 0x75
+//-------------------------------------------------define MPU config---
+//---------------------------------------------------------------------
+#define MPU_ADDR 0x68    // assuming AD0 pin is low
+#define SAMPLE_RATE 50.0 // set sensors sample rate in Hz
 
+
+// ==== Register 26 0x1A– Configuration
+#define CONFIG EXT_SYNC_SET | DLPF_CODE
+// set Digital Low-Pass Filter bandwidth
+#define DLPF_BW 50 // values in { 250, 200, 100, 50, 20, 10, 5}Hz
+//                                   Accelero           |                       Gyro
+#if (DLPF_BW == 250)   // BW = 260Hz | delai =  0.0ms   |   BW = 256Hz | delai =  0.98ms | gyro_sampling_rate = 8kHz
+#define GYRO_OUT_RATE 8000
+#define DLPF_CODE 0x00
+#elif (DLPF_BW == 200) // BW = 184Hz | delai =  2.0ms   |   BW = 188Hz | delai =  1.90ms | gyro_sampling_rate = 1kHz
+#define GYRO_OUT_RATE 1000
+#define DLPF_CODE 0x01
+#elif (DLPF_BW == 100) // BW =  94Hz | delai =  3.0ms   |   BW =  98Hz | delai =  2.80ms | gyro_sampling_rate = 1kHz
+#define GYRO_OUT_RATE 1000
+#define DLPF_CODE 0x02
+#elif (DLPF_BW == 50)  // BW =  44Hz | delai =  4.9ms   |   BW =  42Hz | delai =  4.80ms | gyro_sampling_rate = 1kHz
+#define GYRO_OUT_RATE 1000
+#define DLPF_CODE 0x03
+#elif (DLPF_BW == 20)  // BW =  21Hz | delai =  8.5ms   |   BW =  20Hz | delai =  8.30ms | gyro_sampling_rate = 1kHz
+#define GYRO_OUT_RATE 1000
+#define DLPF_CODE 0x04
+#elif (DLPF_BW == 10)  // BW =  10Hz | delai = 13.8ms   |   BW =  10Hz | delai = 13.40ms | gyro_sampling_rate = 1kHz
+#define GYRO_OUT_RATE 1000
+#define DLPF_CODE 0x05
+#elif (DLPF_BW == 5)   // BW =   5Hz | delai = 19.0ms   |   BW =   5Hz | delai = 18.60ms | gyro_sampling_rate = 1kHz
+#define GYRO_OUT_RATE 1000
+#define DLPF_CODE 0x00
+#endif //
+// set external FSYNC
+#define EXT_SYNC_SET EXT_SYNC_SET_DISABLED
+// values to be selected
+#define  EXT_SYNC_SET_DISABLED     0x00 <<3 
+#define  EXT_SYNC_SET_TEMP_OUT_L   0x01 <<3 
+#define  EXT_SYNC_SET_GYRO_XOUT_L  0x02 <<3 
+#define  EXT_SYNC_SET_GYRO_YOUT_L  0x03 <<3 
+#define  EXT_SYNC_SET_GYRO_ZOUT_L  0x04 <<3 
+#define  EXT_SYNC_SET_ACCEL_XOUT_L 0x05 <<3 
+#define  EXT_SYNC_SET_ACCEL_YOUT_L 0x06 <<3 
+#define  EXT_SYNC_SET_ACCEL_ZOUT_L 0x07 <<3
+// end values
+
+// ==== Register 27 0x1B– Gyroscope Configuration 
+#define GYRO_FULL_SCALE_RANGE 250  // values in { 250, 500, 1000, 2000} DPS
+#if (GYRO_FULL_SCALE_RANGE == 250)
+#define GYRO_CONFIG 0x00 << 3
+#define GYRO_FULL_SCALE_VALUE 250
+#elif (GYRO_FULL_SCALE_RANGE == 500)
+#define GYRO_CONFIG 0x01 << 3
+#define GYRO_FULL_SCALE_VALUE 500
+#elif (GYRO_FULL_SCALE_RANGE == 1000)
+#define GYRO_CONFIG 0x02 << 3
+#define GYRO_FULL_SCALE_VALUE 1000
+#elif (GYRO_FULL_SCALE_RANGE == 2000)
+#define GYRO_CONFIG 0x03 << 3
+#define GYRO_FULL_SCALE_VALUE 2000
+#endif
+
+// ==== Register 28 0x1C– Accelerometer Configuration 
+#define ACCEL_FULL_SCALE_RANGE 2  // values in { 2, 4, 8, 16} G
+#if (ACCEL_FULL_SCALE_RANGE == 2)
+#define ACCEL_CONFIG 0x00 << 3
+#define ACCEL_FULL_SCALE_VALUE 2
+#elif (ACCEL_FULL_SCALE_RANGE == 4)
+#define ACCEL_CONFIG 0x01 << 3
+#define ACCEL_FULL_SCALE_VALUE 4
+#elif (ACCEL_FULL_SCALE_RANGE == 8)
+#define ACCEL_CONFIG 0x02 << 3
+#define ACCEL_FULL_SCALE_VALUE 8
+#elif (ACCEL_FULL_SCALE_RANGE == 16)
+#define ACCEL_CONFIG 0x03 << 3
+#define ACCEL_FULL_SCALE_VALUE 16
+#endif
+
+// ==== Register 35 0x23– FIFO Enable : config which measures will be written into the FIFO
+#define FIFO_SELECTED_SENSOR GYRO_FIFO_EN | ACCEL_FIFO_EN
+// values to be selected :
+#define GYRO_FIFO_EN XG_FIFO_EN | YG_FIFO_EN | ZG_FIFO_EN
+#define TEMP_FIFO_EN 0x80
+#define XG_FIFO_EN 0x40
+#define YG_FIFO_EN 0x20
+#define ZG_FIFO_EN 0x10
+#define ACCEL_FIFO_EN 0x08
+#define SLV2_FIFO_EN 0x04
+#define SLV1_FIFO_EN 0x02
+#define SLV0_FIFO_EN 0x01
+// end values
+
+// ==== Register 55 0x37– INT Pin / Bypass Enable Configuration
+#define INT_PIN_CFG INT_LEVEL | INT_OPEN // Active LO, open drain, pulsed 50us, cleared by read INT status
+// values to be selected :
+#define INT_LEVEL 0x80    // INT pin active LOW
+#define INT_OPEN 0x40     // INT pin open drain
+#define LATCH_INT_EN 0x20 // INT pin held Hi until INT cleared
+#define INT_RD_CLEAR 0x10 // INT pin cleared by any read
+#define FSYNC_INT_LEVEL 0x08
+#define FSYNC_INT_EN 0x04
+#define I2C_BYPASS_EN 0x02
+// end values
+
+// ==== Register 56 0x38– Interrupt Enable
+#define INT_ENABLE DATA_RDY_EN // INT each time a sensor register write occurs
+// values to be selected :
+#define FIFO_OFLOW_EN 0x10
+#define I2C_MST_INT_EN 0x08
+#define DATA_RDY_EN 0x01
+// end values
+
+// ==== Register 58 0x3A– read Interrupt Status
+// values explaining reading
+#define FIFO_OFLOW_INT 0x10
+#define I2C_MST_INT 0x08
+#define DATA_RDY_INT 0x01
+// end values
+
+// ==== Register 104 0x68– Signal Path Reset
+#define SIGNAL_PATH_RESET 0x00 // default: no reset
+// values to be selected
+#define GYRO_RESET 0x04
+#define ACCEL_RESET 0x02
+#define TEMP_RESET 0x01
+// end values
+
+// ==== Register 106 0x6A– User Control
+// config FIFO, I2C, signal path
+#define FIFO_EN 0x40        // enables FIFO operations
+#define I2C_MST_EN 0x20     // enables I2C master mode
+#define I2C_IF_DIS 0x10     // MPU6000 only
+#define FIFO_RESET 0x04     // reset and disables FIFO
+#define I2C_MST_RESET 0x02  // reset I2C master while I2C_MST_EN = 0x00
+#define SIG_COND_RESET 0x01 // reset all sensors processing and registers
+
+// Register 107 0x6B– Power Management 1
+#define PWR_MGMT_1 CLKSEL_INTERNAL
+// values to be selected as CLKSEL
+#define CLKSEL_INTERNAL 0x00
+#define CLKSEL_X_PLL 0x01
+#define CLKSEL_Y_PLL 0x02
+#define CLKSEL_Z_PLL 0x03
+// values to be selected as DEVICE_RESET
+#define DEVICE_RESET 0x80
+// end values
+
+#endif // CONFIG_MPU6050_H
--- mpu6050_i2c/mpu6050.h
+++ mpu6050_i2c/mpu6050.h
@@ -1,46 +1,9 @@
-// #include <stdio.h>
-// #include <string.h>
+#if !defined(MPU6050_H)
+#define MPU6050_H
+
+#include "config_MPU6050.h"
 #include <map>
 #include "hardware/i2c.h"
-
-// configuration of accelerometer
-// typedef struct g_scale
-// {
-//     uint8_t max_g_code;
-//     uint8_t max_g_value;
-// } g_scale_t;
-enum ACCEL_FULL_SCALE_RANGE
-{
-    G_2, // range +/- 2g
-    G_4, // range +/- 4g
-    G_8, // range +/- 8g
-    G_16 // range +/- 16g
-};
-
-// configuration of gyrometer
-// typedef struct gyro_scale
-// {
-//     uint8_t max_gyro_code;
-//     uint16_t max_gyro_value;
-// } gyro_scale_t;
-enum GYRO_FULL_SCALE_RANGE
-{
-    DPS_250,  // range +/- 250 degrees per second
-    DPS_500,  // range +/- 500 degrees per second
-    DPS_1000, // range +/- 1000 degrees per second
-    DPS_2000  // range +/- 2000 degrees per second
-};
-
-// configuration of the Digital Low-Pass Filter
-enum DLPF_BW{     //            Accelero           |                       Gyro
-    HZ_250=0,     // BW = 260Hz | delai =  0.0ms   |   BW = 256Hz | delai =  0.98ms | gyro_sampling_rate = 8kHz 
-    HZ_200=1,     // BW = 184Hz | delai =  2.0ms   |   BW = 188Hz | delai =  1.90ms | gyro_sampling_rate = 1kHz 
-    HZ_100=2,     // BW =  94Hz | delai =  3.0ms   |   BW =  98Hz | delai =  2.80ms | gyro_sampling_rate = 1kHz 
-    HZ_50=3,      // BW =  44Hz | delai =  4.9ms   |   BW =  42Hz | delai =  4.80ms | gyro_sampling_rate = 1kHz 
-    HZ_20=4,      // BW =  21Hz | delai =  8.5ms   |   BW =  20Hz | delai =  8.30ms | gyro_sampling_rate = 1kHz 
-    HZ_10=5,      // BW =  10Hz | delai = 13.8ms   |   BW =  10Hz | delai = 13.40ms | gyro_sampling_rate = 1kHz 
-    HZ_5=6        // BW =   5Hz | delai = 19.0ms   |   BW =   5Hz | delai = 18.60ms | gyro_sampling_rate = 1kHz 
-};
 
 // raw data as they are captured by sensor
 typedef struct RawData
@@ -74,21 +37,22 @@ private:
     float gyro_factor{};
     float temperature_gain = (float)1 / 340;
     float temperature_offset = 36.53;
-    float sample_rate{};
+    float accel_x_offset {};
+    float accel_y_offset {};
+    float accel_z_offset {};
+    float gyro_x_offset {};
+    float gyro_y_offset {};
+    float gyro_z_offset {};
 
     uint8_t single_byte_write(uint8_t reg_addr, uint8_t reg_value);
     uint8_t single_byte_read(uint8_t reg_addr, uint8_t *dest);
     uint8_t burst_byte_read(uint8_t reg_addr, uint8_t *dest, uint8_t len);
-    void set_clock_src_DLPF_and_sample_rate(float rate, DLPF_BW bandwidth);
-    void set_gyro_resolution(GYRO_FULL_SCALE_RANGE range);
-    void set_accel_resolution(ACCEL_FULL_SCALE_RANGE range);
     void read_registers_all_raw_data(RawData_t * raw);
     void read_FIFO_all_raw_data(RawData_t * raw);
-    void read_FIFO_g_accel_raw_data(RawData_t * raw);
 
 public:
+    void read_FIFO_g_accel_raw_data(RawData_t * raw);
     MPU6050();
-    void reset_enable_config_FIFO();
     uint8_t read_interrupt_status();
     uint16_t read_FIFO_count();
 
@@ -96,4 +60,7 @@ public:
     void read_MPU_all_measure_from_registers(MPUData_t * data);
     void read_MPU_all_measure_from_FIFO(MPUData_t * data);
     void read_MPU_g_accel_measures_from_FIFO(MPUData_t * data);
+    void calibrate();
 };
+
+#endif // MPU6050_H
