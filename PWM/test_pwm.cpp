dc3a543d2db22dc8681a4f2e5037be13872a871a
xiansnn <xiansnn@hotmail.com>
2023-04-25 19:32:35 +0200

    create test files for pwm library

---
+++ PWM/test_pwm.cpp
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "pico/stdlib.h"
+#include "pwm_hal.h"
+
+/**
+ * @brief 
+ * 
+ * @return int 
+ */
+int main()
+{
+
+#define CH1 6
+#define CH2 8
+#define CH3 16
+#define CH4 27
+#define STEP_ns 1000
+#define PERIOD_us 20000
+#define PHASE_CORRECT true
+
+    PWM pwm1 = PWM(CH1, CH1+1, STEP_ns, PERIOD_us, PHASE_CORRECT, false, false);
+    pwm1.set_width_nb_of_step(CH1, 1);
+    pwm1.set_duty_cycle(CH1+1, 0.05);
+
+    PWM pwm2 = PWM(CH2, CH2+1, STEP_ns, PERIOD_us, PHASE_CORRECT, false, false);
+    pwm2.set_width_nb_of_step(CH2, 1);
+    pwm2.set_duty_cycle(CH2+1, 0.1);
+    
+    PWM pwm3 = PWM(CH3, CH3+1, STEP_ns, PERIOD_us, PHASE_CORRECT, false, false);
+    pwm3.set_width_nb_of_step(CH3, 1);
+    pwm3.set_duty_cycle(CH3+1, 0.5);
+    
+    PWM pwm4 = PWM(CH4, CH4+1, STEP_ns, PERIOD_us, PHASE_CORRECT, false, false);
+    pwm4.set_width_nb_of_step(CH4, 1);
+    pwm4.set_duty_cycle(CH4+1, 0.9);
+   
+    PWM::StartTogether();
+
+}
---
+++ PWM/test_pwm_irq.cpp
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+// Fade an LED between low and high brightness. An interrupt handler updates
+// the PWM slice's output level each time the counter wraps.
+
+#include "pico/stdlib.h"
+#include <stdio.h>
+#include "pico/time.h"
+#include "hardware/irq.h"
+#include "hardware/pwm.h"
+#include "probe/probe.h"
+#include "pwm_hal.h"
+
+#define LED_PIN 6 
+#define STEP_ns 500
+#define PERIOD_us 100
+#define PHASE_CORRECT false
+
+Probe pr_irq = Probe(4);
+Probe pr_main = Probe(5);
+
+PWM pwm = PWM(LED_PIN,LED_PIN+1,STEP_ns,PERIOD_us,PHASE_CORRECT);
+
+void on_pwm_wrap() {
+    pr_irq.hi();
+    pwm.clear_irq();
+    static bool going_up = true;
+    static float duty_cycle = 0.0;
+    static float duty_cycle_step = 0.01;
+    if (going_up) {
+        duty_cycle += duty_cycle_step;
+        if (duty_cycle >= 1.0) {
+            duty_cycle = 1.0;
+            going_up = false;
+        }
+    } else {
+        duty_cycle -= duty_cycle_step;
+        if (duty_cycle <= 0.0) {
+            duty_cycle = 0.0;
+            going_up = true;
+        }
+    }
+    pwm.set_duty_cycle(LED_PIN+1,duty_cycle);
+    pr_irq.lo();
+}
+
+
+int main() {
+    pwm.set_width_nb_of_step(LED_PIN,1);
+    pwm.set_duty_cycle(LED_PIN+1, 0.01);
+    pwm.set_irq(on_pwm_wrap);
+    pwm.start(true);
+
+    while (true){
+        pr_main.hi();
+        sleep_us(10);
+        pr_main.lo();
+        sleep_us(90);
+    }
+}
