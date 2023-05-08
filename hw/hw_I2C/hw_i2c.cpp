90a56fe6e6875a4676e713af14eebed91b954d47
xiansnn <xiansnn@hotmail.com>
2023-05-08 18:51:01 +0200

    add debug + timeout on read ans  write

--- hw/hw_I2C/hw_i2c.cpp
+++ hw/hw_I2C/hw_i2c.cpp
@@ -97,8 +97,10 @@ int hw_I2C_master::single_byte_read(uint8_t slave_address, uint8_t slave_mem_add
 int hw_I2C_master::burst_byte_read(uint8_t slave_address, uint8_t slave_mem_addr, uint8_t *dest, uint8_t len)
 {
     uint8_t cmd_buf[]{slave_mem_addr};
-    i2c_write_blocking(this->i2c, slave_address, cmd_buf, 1, true);
-    int nb = i2c_read_blocking(this->i2c, slave_address, dest, len, false);
+    // i2c_write_blocking(this->i2c, slave_address, cmd_buf, 1, true);
+    i2c_write_timeout_us(this->i2c,slave_address,cmd_buf,1,true,2000);
+    // int nb = i2c_read_blocking(this->i2c, slave_address, dest, len, false);
+    int nb = i2c_read_timeout_us(this->i2c, slave_address, dest, len, false,2000);
     return nb;
 }
 
