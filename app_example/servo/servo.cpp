dcfe57fa62517d57d44f61cf88a9676edc80b090
xiansnn <xiansnn@hotmail.com>
2023-05-11 16:45:25 +0200

    added wait for MPU data ready

--- app_example/servo/servo.cpp
+++ app_example/servo/servo.cpp
@@ -5,50 +5,59 @@
 
 //-----------------------------------------------------------------------------
 //                             MPU6050 I2C bus and sensor config
-config_master_i2c i2c_bus_cfg{
+config_master_i2c_t i2c_bus_cfg{
     .i2c = i2c0,
     .sda_pin = 8,
     .scl_pin = 9,
-    .baud_rate = I2C_STANDARD_MODE
-};
+    .baud_rate = I2C_STANDARD_MODE};
+
 config_MPU6050_t sensor_config{
     .SAMPLE_RATE = 50,
-    .DLPF_BW = 5
-};
+    .DLPF_BW = 5,
+    // .FIFO_SELECTED_SENSORS = GYRO_FIFO_EN | ACCEL_FIFO_EN,
+    // .FIFO_OP = FIFO_EN
+    .FIFO_SELECTED_SENSORS = FIFO_NO_SELECTION,
+    .FIFO_OP = FIFO_DISABLE};
 //-----------------------------------------------------------------------------
 //                             SG90 config
 config_sg90_t motor_config{
     .command_pin = 7,
-    .sync_pin = 6
-};
+    .sync_pin = 6,
+    .pos_min_degree = -90,
+    .pos_max_degree = +90};
 //-----------------------------------------------------------------------------
 
 Probe pr_D4 = Probe(4);
 Probe pr_D5 = Probe(5);
-
 
 int main()
 {
     stdio_init_all();
     hw_I2C_master bus_master = hw_I2C_master(i2c_bus_cfg);
     SG90 motor = SG90(motor_config);
-    MPU6050 sensor = MPU6050(&bus_master,sensor_config);
+    MPU6050 sensor = MPU6050(&bus_master, sensor_config);
+    motor.set_pos((motor_config.pos_max_degree + motor_config.pos_min_degree) / 2); // set pos at mid range
+    sleep_ms(5000);
     MPUData_t measures;
-    float dev;
 
     while (true)
     {
-        pr_D4.hi();
-        sensor.read_MPU_all_measure_from_registers(&measures);
-        pr_D4.lo();
-        dev = measures.g_z;
-        // printf("dev : %+000.1f \n", dev);
-        int pos = motor_config.pos_max_degree * dev;
-        pr_D5.hi();
-        motor.set_pos(pos);
-        pr_D5.lo();
-        sleep_ms(20);
+        if (sensor.is_data_ready())
+        {
+            float dev;
+            pr_D4.hi();
+            sensor.read_MPU_all_measure_from_registers(&measures);
+
+            pr_D4.lo();
+            dev = measures.g_z;
+            // printf("dev : %+000.1f \n", dev);
+            int pos = motor_config.pos_max_degree * dev;
+            pr_D5.hi();
+            motor.set_pos(pos);
+            pr_D5.lo();
+        }
+        sleep_ms(5);
     }
-    
+
     return 0;
 }
