3ec2575ee96c66aeb542279bf52b823464b6e95a
Chris <42316927+xiansnn@users.noreply.github.com>
2023-09-20 16:39:56 +0200

    added line, hline, vline and rect primitives

--- device/device_ssd1306/framebuffer.cpp
+++ device/device_ssd1306/framebuffer.cpp
@@ -1,21 +1,34 @@
 #include "framebuffer.h"
+#include <string.h>
+#define BYTE_SIZE 8
 
 Framebuffer::Framebuffer(uint8_t buffer[], size_t width, size_t height, Framebuffer_format format)
 {
-    assert( format == Framebuffer_format::MONO_VLSB);
+    assert(format == Framebuffer_format::MONO_VLSB); // works only for MONO_VLSB devices
+    this->format = format;
     this->buffer = buffer;
     this->frame_height = height;
-    this->frame_width = width;
+    this->frame_width = width; // MONO_VLDB => 1 Byte = 1 column of 8 pixel
+    size_t page_nb = height/BYTE_SIZE;
+    if (height%BYTE_SIZE != 0)page_nb +=1;
+    this->buffer_size = width * page_nb;
 }
 
 void Framebuffer::fill(Framebuffer_color c)
 {
+    assert(this->format == Framebuffer_format::MONO_VLSB);
+    // uint8_t image[SSD1306_BUF_LEN];
+    int pattern;
+    if (c == Framebuffer_color::black)
+        pattern = 0x00;
+    else
+        pattern = 0xFF;
+    memset(this->buffer, pattern, this->buffer_size);
 }
 
 void Framebuffer::pixel(uint8_t x, uint8_t y, Framebuffer_color c)
 {
-    assert(x >= 0 && x < this->frame_width && y >= 0 && y < this->frame_height);
-
+    assert(format == Framebuffer_format::MONO_VLSB); // works only if MONO_VLSB
     // The calculation to determine the correct bit to set depends on which address
     // mode we are in. This code assumes horizontal
 
@@ -25,13 +38,12 @@ void Framebuffer::pixel(uint8_t x, uint8_t y, Framebuffer_color c)
 
     // This code could be optimised, but is like this for clarity. The compiler
     // should do a half decent job optimising it anyway.
-
-    const int BytesPerRow = this->frame_width; // x pixels, 1bpp, but each row is 8 pixel high, so (x / 8) * 8
-
+    assert(x >= 0 && x < this->frame_width && y >= 0 && y < this->frame_height); // avoid drawing outside the framebuffer
+    const int BytesPerRow = this->frame_width;                                   // x pixels, 1bpp, but each row is 8 pixel high, so (x / 8) * 8
     int byte_idx = (y / 8) * BytesPerRow + x;
     uint8_t byte = this->buffer[byte_idx];
 
-    if (c==Framebuffer_color::white)
+    if (c == Framebuffer_color::white)
         byte |= 1 << (y % 8);
     else
         byte &= ~(1 << (y % 8));
@@ -39,12 +51,16 @@ void Framebuffer::pixel(uint8_t x, uint8_t y, Framebuffer_color c)
     this->buffer[byte_idx] = byte;
 }
 
-void Framebuffer::hline(uint8_t x, uint8_t y, Framebuffer_color c)
+void Framebuffer::hline(uint8_t x, uint8_t y, size_t w, Framebuffer_color c)
 {
+    for (size_t i = 0; i < w; i++)
+        this->pixel(x + i, y, c);
 }
 
-void Framebuffer::vline(uint8_t x, uint8_t y, Framebuffer_color c)
+void Framebuffer::vline(uint8_t x, uint8_t y, size_t h, Framebuffer_color c)
 {
+    for (size_t i = 0; i < h; i++)
+        this->pixel(x, y + i, c);
 }
 
 void Framebuffer::line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, Framebuffer_color c)
@@ -58,7 +74,7 @@ void Framebuffer::line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, Framebuff
 
     while (true)
     {
-        this->pixel( x0, y0, c);
+        this->pixel(x0, y0, c);
         if (x0 == x1 && y0 == y1)
             break;
         e2 = 2 * err;
@@ -77,11 +93,23 @@ void Framebuffer::line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, Framebuff
     }
 }
 
-void Framebuffer::rect(uint8_t x, uint8_t y, size_t w, size_t h, Framebuffer_color c, bool fill)
+void Framebuffer::rect(uint8_t x, uint8_t y, size_t w, size_t h, bool fill, Framebuffer_color c)
 {
+    if (!fill)
+    {
+        hline(x, y, w, c);
+        hline(x, y + h - 1, w, c);
+        vline(x, y, h, c);
+        vline(x + w - 1, y, h, c);
+    }
+    else
+    {
+        for (size_t i = 0; i < h; i++)
+            hline(x, y + i, w, c);
+    }
 }
 
-void Framebuffer::ellipse(uint8_t x, uint8_t y, uint8_t xr, uint8_t yr, Framebuffer_color c, bool fill, uint8_t m)
+void Framebuffer::ellipse(uint8_t x, uint8_t y, uint8_t xr, uint8_t yr, bool fill, uint8_t m, Framebuffer_color c)
 {
 }
 
--- device/device_ssd1306/framebuffer.h
+++ device/device_ssd1306/framebuffer.h
@@ -1,3 +1,6 @@
+#if !defined(FRAMEBUFFER_H)
+#define FRAMEBUFFER_H
+
 #include "pico/stdlib.h"
 #include <string>
 
@@ -18,17 +21,22 @@ class Framebuffer
 {
 private:
     /* data */
-    uint8_t * buffer;
     size_t frame_width;
     size_t frame_height;
+    size_t buffer_size;
+    Framebuffer_format format;
 public:
+    uint8_t * buffer;
+
     Framebuffer(uint8_t buffer[], size_t width, size_t height, Framebuffer_format format);
     void fill(Framebuffer_color c);
-    void pixel(uint8_t x, uint8_t y, Framebuffer_color c);
-    void hline(uint8_t x, uint8_t y, Framebuffer_color c);
-    void vline(uint8_t x, uint8_t y, Framebuffer_color c);
-    void line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, Framebuffer_color c);
-    void rect(uint8_t x, uint8_t y, size_t w, size_t h, Framebuffer_color c, bool fill);
-    void ellipse(uint8_t x, uint8_t y, uint8_t xr, uint8_t yr, Framebuffer_color c, bool fill, uint8_t m);
-    void text(std::string s, uint8_t x, uint8_t y, Framebuffer_color c);
+    void pixel(uint8_t x, uint8_t y, Framebuffer_color c = Framebuffer_color::white);
+    void hline(uint8_t x, uint8_t y, size_t w, Framebuffer_color c= Framebuffer_color::white);
+    void vline(uint8_t x, uint8_t y, size_t h, Framebuffer_color c= Framebuffer_color::white);
+    void line(uint8_t x1, uint8_t y1, uint8_t x2, uint8_t y2, Framebuffer_color c= Framebuffer_color::white);
+    void rect(uint8_t x, uint8_t y, size_t w, size_t h, bool fill=false, Framebuffer_color c= Framebuffer_color::white);
+    void ellipse(uint8_t x, uint8_t y, uint8_t xr, uint8_t yr, bool fill=false, uint8_t m= 0b1111, Framebuffer_color c= Framebuffer_color::white);
+    void text(std::string s, uint8_t x, uint8_t y, Framebuffer_color c= Framebuffer_color::white);
 };
+
+#endif // FRAMEBUFFER_H
