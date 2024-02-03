c56f49848ca0bcb649455904aa038eb4b37cf276
Chris <42316927+xiansnn@users.noreply.github.com>
2024-02-03 11:36:58 +0100

    this version works fine!

--- device/device_KY_040/test_ky_040.cpp
+++ device/device_KY_040/test_ky_040.cpp
@@ -13,21 +13,24 @@
 
 Probe pr_D5 = Probe(5);
 Probe pr_D4 = Probe(4);
+Probe pr_D1 = Probe(1);
 
 switch_button_config_t sw_conf{
     .debounce_delay_us = 1000,
     .long_press_delay_us = 500000};
 
 switch_button_config_t clk_conf{
-    .debounce_delay_us = 100,
+    .debounce_delay_us = 1000,
 };
 
 void call_back(uint gpio, uint32_t event_mask);
 
 KY040_IRQ encoder = KY040_IRQ(ENCODER_CLK, ENCODER_DT, &call_back, clk_conf);
 ControlledValue val = ControlledValue(MIN_VALUE, MAX_VALUE);
+
 void call_back(uint gpio, uint32_t event_mask)
 {
+    pr_D5.hi();
     EncoderEvent encoder_event = encoder.get_encoder_event();
     switch (encoder_event)
     {
@@ -40,6 +43,7 @@ void call_back(uint gpio, uint32_t event_mask)
     default:
         break;
     }
+    pr_D5.lo();
 }
 
 float a = 79. / (MAX_VALUE - MIN_VALUE);
@@ -54,60 +58,34 @@ int main()
     {
         if (val.has_changed)
         {
-            printf("LOOP......: %2d %*c\n", val.get_value(), get_tab(val.get_value()), '|');
-            sleep_ms(20);
+            printf("LOOP: %2d %*c\n", val.get_value(), get_tab(val.get_value()), '|');
             val.clear_change_flag();
         }
 
         pr_D4.hi();
         SwitchButtonEvent sw_event = sw.get_event();
-        pr_D4.lo();
 
         switch (sw_event)
         {
         case SwitchButtonEvent::PUSH:
-            val.reset_value();
-            printf("......push: %2d %+*c\n", val.get_value(), get_tab(val.get_value()), '|');
+            printf("............push\n");
             break;
         case SwitchButtonEvent::LONG_PUSH:
-            val.reset_value();
-            printf(".long.push: %2d %+*c\n", val.get_value(), get_tab(val.get_value()), '|');
+            printf(".......long.push\n");
             break;
         case SwitchButtonEvent::RELEASED_AFTER_LONG_TIME:
-            val.reset_value();
-            printf(".long..rel: %2d %+*c\n", val.get_value(), get_tab(val.get_value()), '|');
+            printf(".......long..rel\n");
             break;
         case SwitchButtonEvent::RELEASED_AFTER_SHORT_TIME:
             val.reset_value();
-            printf(".short.rel: %2d %+*c\n", val.get_value(), get_tab(val.get_value()), '|');
-            break;
-
-        default:
-            break;
-        }
-
-        pr_D5.hi();
-        EncoderEvent encoder_event = encoder.get_encoder_event();
-
-        switch (encoder_event)
-        {
-        case EncoderEvent::INCREMENT:
-            val.increment_value();
-            printf("%2d %+*c\n", val.get_value(), val.get_value(), '|');
-            break;
-        case EncoderEvent::DECREMENT:
-            val.decrement_value();
-            printf("%2d %+*c\n", val.get_value(), val.get_value(), '|');
+            printf(".......short.rel\n");
             break;
         default:
             break;
         }
-        pr_D5.lo();
+        pr_D4.lo();
 
-        sleep_ms(5);
-        /**
-         * caution : depending on the period of the scan, the value of the DT pahse of the encoder may be red when the true value has already changed.
-         */
+        sleep_ms(20);
     }
 
     return 0;
