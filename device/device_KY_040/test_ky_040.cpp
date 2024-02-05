6157433f991aec1eb26ab42736346ffd2f2d73d8
Chris <42316927+xiansnn@users.noreply.github.com>
2024-02-05 18:43:05 +0100

    FIX: error on rotate direction.  disabling IQ when entering ISR

--- device/device_KY_040/test_ky_040.cpp
+++ device/device_KY_040/test_ky_040.cpp
@@ -9,13 +9,12 @@
 #define ENCODER_CLK_GPIO 26
 #define ENCODER_DT_GPIO 21
 
-
 Probe pr_D5 = Probe(5);
 Probe pr_D4 = Probe(4);
-
+Probe pr_D3 = Probe(3);
 
 switch_button_config_t central_switch_conf{
-    .debounce_delay_us = 1000,
+    .debounce_delay_us = 2000,
     .long_release_delay_us = 3000000,
     .long_push_delay_us = 500000};
 
@@ -29,50 +28,51 @@ KY040_IRQ encoder = KY040_IRQ(ENCODER_CLK_GPIO, ENCODER_DT_GPIO, shared_irq_call
 
 void shared_irq_call_back(uint gpio, uint32_t event_mask)
 {
+    pr_D3.hi();
     switch (gpio)
     {
     case ENCODER_CLK_GPIO:
-        encoder.process_encoder_event();
+        gpio_set_irq_enabled_with_callback(ENCODER_CLK_GPIO, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL, false, shared_irq_call_back);
+        encoder.interrupt_service_routine();
+        gpio_set_irq_enabled_with_callback(ENCODER_CLK_GPIO, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL, true, shared_irq_call_back);
         break;
 
     default:
         printf("unknown IRQ\n");
         break;
     }
+    pr_D3.lo();
 };
 
-int get_tab(ControlledValue* val)
+int display_value(ControlledValue *val)
 {
-#define MAX_TAB_WIDTH 80.
-    float a = (MAX_TAB_WIDTH - 1.) / (val->get_max_value() - val->get_min_value());
+#define MAX_WIDTH 80.
+    float a = (MAX_WIDTH - 1.) / (val->get_max_value() - val->get_min_value());
     float b = 1 - a * val->get_min_value();
     return a * val->get_value() + b;
 };
 
-void shared_irq_call_back(uint gpio, uint32_t event_mask);
 int main()
 {
     stdio_init_all();
     SwitchButton central_switch = SwitchButton(CENTRAL_SWITCH_GPIO, central_switch_conf);
-    ControlledValue val = ControlledValue(-20, +20);
-    ControlledValue val2 = ControlledValue(5,20);
-    ControlledValue val3 = ControlledValue(-20,-5);
-    encoder.add_cntrl_value(&val3);
-    ControlledValue* current_cntrl_value = encoder.get_current_cntrl_value();
+    ControlledValue val1 = ControlledValue(-20, +20);
+    ControlledValue val2 = ControlledValue(5, 20);
+    ControlledValue val3 = ControlledValue(-20, -5);
+    ControlledValue *current_cntrl_value = encoder.set_cntrl_value(&val1);
 
     while (true)
     {
         if (current_cntrl_value->has_changed)
         {
             pr_D5.hi();
-            printf("LOOP: %2d %*c\n", current_cntrl_value->get_value(), get_tab(current_cntrl_value), '|');
+            printf("LOOP: %2d %*c\n", current_cntrl_value->get_value(), display_value(current_cntrl_value), '|');
             current_cntrl_value->clear_change_flag();
             pr_D5.lo();
         }
 
         pr_D4.hi();
-        SwitchButtonEvent sw_event = central_switch.get_event();
-
+        SwitchButtonEvent sw_event = central_switch.get_sample_event();
         switch (sw_event)
         {
         case SwitchButtonEvent::PUSH:
