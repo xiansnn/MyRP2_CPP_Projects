14d502a619c0c2efc5446841c8453997925346ba
Chris <42316927+xiansnn@users.noreply.github.com>
2024-02-06 11:08:13 +0100

    reworked logic of changing cntrl_value

--- device/device_KY_040/test_ky_040.cpp
+++ device/device_KY_040/test_ky_040.cpp
@@ -2,6 +2,7 @@
 #include "ky_040.h"
 
 #include <string>
+#include <vector>
 
 #include "controlled_value.h"
 
@@ -16,15 +17,16 @@ Probe pr_D3 = Probe(3);
 switch_button_config_t central_switch_conf{
     .debounce_delay_us = 2000,
     .long_release_delay_us = 3000000,
-    .long_push_delay_us = 500000};
+    .long_push_delay_us = 1000000,
+    .active_lo = true};
 
 switch_button_config_t encoder_clk_conf{
     .debounce_delay_us = 1000,
 };
 
 void shared_irq_call_back(uint gpio, uint32_t event_mask);
-KY040_IRQ encoder = KY040_IRQ(ENCODER_CLK_GPIO, ENCODER_DT_GPIO, shared_irq_call_back,
-                              encoder_clk_conf);
+KY040 encoder = KY040(ENCODER_CLK_GPIO, ENCODER_DT_GPIO, shared_irq_call_back,
+                      encoder_clk_conf);
 
 void shared_irq_call_back(uint gpio, uint32_t event_mask)
 {
@@ -32,9 +34,10 @@ void shared_irq_call_back(uint gpio, uint32_t event_mask)
     switch (gpio)
     {
     case ENCODER_CLK_GPIO:
-        gpio_set_irq_enabled_with_callback(ENCODER_CLK_GPIO, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL, false, shared_irq_call_back);
-        encoder.interrupt_service_routine();
-        gpio_set_irq_enabled_with_callback(ENCODER_CLK_GPIO, GPIO_IRQ_EDGE_RISE|GPIO_IRQ_EDGE_FALL, true, shared_irq_call_back);
+        gpio_set_irq_enabled_with_callback(ENCODER_CLK_GPIO, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, false, shared_irq_call_back);
+        if (event_mask == GPIO_IRQ_EDGE_FALL)
+            encoder.interrupt_service_routine();
+        gpio_set_irq_enabled_with_callback(ENCODER_CLK_GPIO, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, shared_irq_call_back);
         break;
 
     default:
@@ -51,6 +54,13 @@ int display_value(ControlledValue *val)
     float b = 1 - a * val->get_min_value();
     return a * val->get_value() + b;
 };
+static int current_index = 0;
+ControlledValue *next_cntrl_value_index(std::vector<ControlledValue *> values)
+{
+    current_index++;
+    current_index = current_index % values.size();
+    return values[current_index];
+}
 
 int main()
 {
@@ -59,14 +69,18 @@ int main()
     ControlledValue val1 = ControlledValue(-20, +20);
     ControlledValue val2 = ControlledValue(5, 20);
     ControlledValue val3 = ControlledValue(-20, -5);
-    ControlledValue *current_cntrl_value = encoder.set_cntrl_value(&val1);
+
+    std::vector<ControlledValue *> cntrl_values = {&val1, &val2, &val3};
+
+    ControlledValue *current_cntrl_value = encoder.set_cntrl_value(cntrl_values[0]);
 
     while (true)
     {
+
         if (current_cntrl_value->has_changed)
         {
             pr_D5.hi();
-            printf("LOOP: %2d %*c\n", current_cntrl_value->get_value(), display_value(current_cntrl_value), '|');
+            printf("LOOP[%d]: %2d %*c\n", current_index, current_cntrl_value->get_value(), display_value(current_cntrl_value), '|');
             current_cntrl_value->clear_change_flag();
             pr_D5.lo();
         }
@@ -76,17 +90,19 @@ int main()
         switch (sw_event)
         {
         case SwitchButtonEvent::PUSH:
+            // next_cntrl_value_index(cntrl_values);
             printf("............push\n");
-            break;
-        case SwitchButtonEvent::LONG_PUSH:
-            printf(".......long.push\n");
-            break;
-        case SwitchButtonEvent::RELEASED_AFTER_LONG_TIME:
-            printf(".......long..rel\n");
             break;
         case SwitchButtonEvent::RELEASED_AFTER_SHORT_TIME:
             current_cntrl_value->reset_value();
             printf(".......short.rel\n");
+            break;
+        case SwitchButtonEvent::LONG_PUSH:
+            current_cntrl_value = encoder.set_cntrl_value(next_cntrl_value_index(cntrl_values));
+            printf(".......long.push\n");
+            break;
+        case SwitchButtonEvent::RELEASED_AFTER_LONG_TIME:
+            printf(".......long..rel\n");
             break;
         default:
             break;
