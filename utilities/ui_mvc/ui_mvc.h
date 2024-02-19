8f799a942fe21bbdd3a7bed5a42a5c22039f56ba
Chris <42316927+xiansnn@users.noreply.github.com>
2024-02-19 15:03:16 +0100

    cleaning and add print_console widget

--- utilities/ui_mvc/ui_mvc.h
+++ utilities/ui_mvc/ui_mvc.h
@@ -4,40 +4,17 @@
 #include <map>
 #include "pico/stdlib.h"
 
-class UI_ControlledObject;
-class UI_Widget;
-
-class UI_Controller
-{
-private:
-    /* data */
-public:
-    uint8_t id;
-    UI_ControlledObject *active_controlled_object;
-
-    UI_Controller(uint8_t id);
-    ~UI_Controller();
-
-    UI_ControlledObject *set_active_controlled_object(UI_ControlledObject *cntrl_obj);
-};
-
 class UI_ControlledObject
 {
 private:
-    /* data */
 public:
-    uint8_t id;
-    bool has_changed{false};
-    std::map<uint8_t, UI_Controller *> controllers;
-    std::map<uint8_t, UI_Widget *> widgets;
-
     UI_ControlledObject(uint8_t id);
     ~UI_ControlledObject();
+
+    uint8_t id;
+    bool has_changed{false};
+
     void clear_change_flag();
-
-    void add_widget(UI_Widget *widget);
-    void add_controller(UI_Controller *controller);
-
     virtual void on_push() = 0;
     virtual void on_long_push() = 0;
     virtual void on_short_release() = 0;
@@ -47,18 +24,30 @@ public:
     virtual void reset() = 0;
 };
 
+class UI_Controller
+{
+private:
+public:
+    UI_Controller(uint8_t id);
+    ~UI_Controller();
+
+    uint8_t id;
+    UI_ControlledObject *active_controlled_object;
+
+    UI_ControlledObject *set_active_controlled_object(UI_ControlledObject *cntrl_obj);
+};
+
 class UI_Widget
 {
 private:
-    /* data */
 public:
-    uint8_t id;
-    std::map<uint8_t, UI_ControlledObject *> displayed_objects;
-
     UI_Widget(uint8_t id);
     ~UI_Widget();
 
-    void add_displayed_object(UI_ControlledObject *displayed_object);
+    uint8_t id;
+    UI_ControlledObject *active_displayed_object;
+
+    UI_ControlledObject *set_active_displayed_object(UI_ControlledObject *displayed_object);
 };
 
 #endif // UI_MVC_H
