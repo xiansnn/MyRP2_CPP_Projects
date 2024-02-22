b710320df628c296645d1255a68f063340086469
Chris <42316927+xiansnn@users.noreply.github.com>
2024-02-22 18:16:50 +0100

    first step of test multiple bar

--- sw/widgets/test_multiple_bar.cpp
+++ sw/widgets/test_multiple_bar.cpp
@@ -2,7 +2,9 @@
 #include "ssd1306.h"
 #include "widget_bar.h"
 
-#include "probe.h"
+#include "focus_manager.h"
+#include "print_console_line.h"
+
 
 
 #include <string>
@@ -14,7 +16,16 @@
 #define SSD1306_I2C_SDA_GPIO 8
 #define SSD1306_I2C_SCL_GPIO 9
 
-Probe pr_D1 = Probe(1);
+#define ENCODER_ID 11
+#define CENTRAL_SWITCH_ID 12
+
+#define CONTROLLED_VAL1_ID 21
+#define CONTROLLED_VAL2_ID 22
+#define CONTROLLED_VAL3_ID 23
+
+#define CONSOLE_WIDGET_ID 31
+
+
 
 config_master_i2c_t cfg_i2c{
     .i2c = i2c0,
@@ -31,14 +42,6 @@ config_SSD1306_t cfg_ssd1306{
     .frequency_divider = 1,
     .frequency_factor = 0};
 
-config_switch_button_t cfg_central_switch{
-    .debounce_delay_us = 5000,
-    .long_release_delay_us = 3000000,
-    .long_push_delay_us = 1000000,
-    .active_lo = true};
-
-config_switch_button_t cfg_encoder_clk{
-    .debounce_delay_us = 5000};
 
 config_bar_widget_t cfg_bar{
     .width = 128,
@@ -47,10 +50,17 @@ config_bar_widget_t cfg_bar{
     .with_label = true,
     .font = font_5x8};
 
-void shared_irq_call_back(uint gpio, uint32_t event_mask);
+config_switch_button_t central_switch_conf{
+    .debounce_delay_us = 5000,
+    .long_release_delay_us = 1000000,
+    .long_push_delay_us = 1000000,
+    .active_lo = true};
 
-KY040Encoder encoder = KY040Encoder(ENCODER_CLK_GPIO, ENCODER_DT_GPIO, shared_irq_call_back,
-                      cfg_encoder_clk);
+config_switch_button_t cfg_encoder_clk{
+    .debounce_delay_us = 5000};
+void shared_irq_call_back(uint gpio, uint32_t event_mask);
+KY040Encoder encoder = KY040Encoder(ENCODER_ID, ENCODER_CLK_GPIO, ENCODER_DT_GPIO,
+                                    shared_irq_call_back, cfg_encoder_clk);
 
 void shared_irq_call_back(uint gpio, uint32_t event_mask)
 {
@@ -63,17 +73,13 @@ void shared_irq_call_back(uint gpio, uint32_t event_mask)
     default:
         printf("unknown IRQ\n");
         break;
-    }
-};
-
-static int current_index = 0;
-
-ControlledValue *next_cntrl_value_index(std::vector<ControlledValue *> values)
-{
-    current_index++;
-    current_index = current_index % values.size();
-    return values[current_index];
+    };
 }
+
+FocusManager focus_manager = FocusManager();
+DisplayControlledValueOnTerminal console = DisplayControlledValueOnTerminal(CONSOLE_WIDGET_ID);
+
+
 
 int main()
 {
@@ -81,63 +87,51 @@ int main()
     hw_I2C_master master = hw_I2C_master(cfg_i2c);
     SSD1306 display = SSD1306(&master, cfg_ssd1306);
     display.clear_pixel_buffer_and_show_full_screen();
+    SwitchButton central_switch = SwitchButton(CENTRAL_SWITCH_ID, CENTRAL_SWITCH_GPIO, central_switch_conf);
 
-    SwitchButton central_switch = SwitchButton(CENTRAL_SWITCH_GPIO, cfg_central_switch);
+    encoder.set_active_controlled_object(&focus_manager);
+    console.set_active_displayed_object(&focus_manager);
+    focus_manager.set_current_controller(&encoder);
+    focus_manager.set_current_widget(&console);
 
-    ControlledValue val0 = ControlledValue(876, 1079, 1, true);
-    ControlledValue val1 = ControlledValue(-10, +10);
-    ControlledValue val2 = ControlledValue(5, 25);
-    ControlledValue val3 = ControlledValue(-25, -5);
+    ControlledValue val1 = ControlledValue(CONTROLLED_VAL1_ID, -10, +10);
+    focus_manager.add_controlled_object(&val1);
+    val1.set_current_controller(&encoder);
+    val1.set_current_widget(&console);
 
-    val0.has_changed = true;
-    val1.has_changed = true;
-    val2.has_changed = true;
-    val3.has_changed = true;
+    ControlledValue val2 = ControlledValue(CONTROLLED_VAL2_ID, 5, 25);
+    focus_manager.add_controlled_object(&val2);
+    val2.set_current_controller(&encoder);
+    val2.set_current_widget(&console);
 
-    Framebuffer header = Framebuffer(80, 8);
-    Bar bar1 = Bar(&val1, cfg_bar);
-    Bar bar2 = Bar(&val2, cfg_bar);
-    Bar bar3 = Bar(&val3, cfg_bar);
+    ControlledValue val3 = ControlledValue(CONTROLLED_VAL3_ID, -25, -5);
+    focus_manager.add_controlled_object(&val3);
 
-    std::vector<ControlledValue *> cntrl_values = {&val1, &val2, &val3};
+    UI_ControlledObject *current_cntrl_obj = &focus_manager;
+    encoder.set_active_controlled_object(current_cntrl_obj);
 
-    ControlledValue *current_cntrl_value = encoder.set_controlled_object(cntrl_values[0]);
+    console.set_active_displayed_object(current_cntrl_obj);
 
     while (true)
     {
-        pr_D1.pulse_us(1);
-        if (current_cntrl_value->has_changed)
+        if (focus_manager.active_controlled_object->value_has_changed)
         {
-            current_cntrl_value->clear_change_flag();
-            header.clear_text_buffer();
-            sprintf(header.text_buffer, "%5.1f MHz", (float)val0.get_value() / 10);
-            header.print_text();
-            display.show(&header, 40, 0);
-            bar1.draw();
-            display.show(&bar1, 0, 16);
-            bar2.draw();
-            display.show(&bar2, 0, 32);
-            bar3.draw();
-            display.show(&bar3, 0, 48);
+            console.draw();
+            focus_manager.active_controlled_object->clear_value_change_flag();
         }
-        SwitchButtonEvent sw_event = central_switch.process_sample_event();
-        switch (sw_event)
+        focus_manager.process_control_event(&central_switch);
+        if (focus_manager.active_controlled_object_has_changed)
         {
-        case SwitchButtonEvent::PUSH:
-            break;
-        case SwitchButtonEvent::RELEASED_AFTER_SHORT_TIME:
-            current_cntrl_value = encoder.set_controlled_object(next_cntrl_value_index(cntrl_values));
-            break;
-        case SwitchButtonEvent::LONG_PUSH:
-            break;
-        case SwitchButtonEvent::RELEASED_AFTER_LONG_TIME:
-            current_cntrl_value->reset_value();
-            break;
-        default:
-            break;
+            encoder.set_active_controlled_object(focus_manager.active_controlled_object);
+            console.set_active_displayed_object(focus_manager.active_controlled_object);
+            console.draw();
+            focus_manager.clear_active_controlled_object_change_flag();
         }
+
         sleep_ms(20);
     }
 
+
+    
     return 0;
 }
