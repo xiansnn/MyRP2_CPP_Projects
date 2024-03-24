0d85cde775dd1f3cbf1d095321f76c40ba9ab734
Chris <42316927+xiansnn@users.noreply.github.com>
2024-03-24 17:24:11 +0100

    to replace UI_ControlledObject  by SingleControlledValue :

--- utilities/cntrl_value/controlled_value.h
+++ utilities/cntrl_value/controlled_value.h
@@ -14,4 +14,34 @@ public:
     void set_value_clipped(int new_value);
 };
 
+class SingleControlledValue : public AbstractModelObject
+{
+private:
+
+protected:
+    int value;
+    int min_value;
+    int max_value;
+    int increment{1};
+    bool wrap;
+
+public:
+    SingleControlledValue(int min_value = 0, int max_value = 10, bool wrap = false, int increment = 1);
+    ~SingleControlledValue();
+
+    virtual int get_min_value();
+    virtual void set_min_value(int value);
+    virtual int get_max_value();
+    virtual void set_max_value(int value);
+    virtual int get_value();
+    virtual void increment_value();
+    virtual void decrement_value();
+
+    virtual void set_value_clipped(int new_value);
+    virtual void process_control_event(ControlEvent);
+
+
+};
+
+
 #endif // CNTRL_VALUE_H
